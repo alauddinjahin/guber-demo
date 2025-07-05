2df0f5fedcea77f50f2a6c237f0b10dceb98841f
alauddinapex <md.alauddin@apexproperty.com.bd>
2025-07-05 21:04:27 +0600

    Completed brand assignment task

--- src/test-assignment-tasks.ts
+++ src/test-assignment-tasks.ts
@@ -5,6 +5,7 @@ import { countryCodes } from "./config/enums"
 import { sources } from "./sites/sources"
 
 export async function runTest() {
+    console.log('---------- processing ......');
     const { data:formattedData, message} = await assignBrandIfKnown(countryCodes.lt, sources.APO)
     console.log(message);
     console.log("Please check a `brandAssignments.json` file in root DIR.");
